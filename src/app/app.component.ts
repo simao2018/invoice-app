e73b72aba851cf17265666d50b628f62c35592b2
chancelsimo <chancelsim@outlook.fr>
2025-06-06 17:58:27 +0200

    wp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Item, PdfService } from './pdf.service';
 
 @Component({
   selector: 'app-root',
